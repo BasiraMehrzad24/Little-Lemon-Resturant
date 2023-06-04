faee035988ef4f21ee085e70f01be98b757b4dda
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2023-06-04 09:55:11 +0430

    meta tag are added

---
+++ littlelemon/src/Footer.js
@@ -0,0 +1,9 @@
+<footer>
+<div class="footer">
+    <h1>
+        Hello dear all
+    </h1>
+
+</div>
+
+</footer>
---
+++ littlelemon/src/Nav.js
@@ -0,0 +1,28 @@
+<nav className="App">
+    <ul>
+        <li>
+            <img src="public/loho192.png">
+            </img>
+        </li>
+
+        <li>
+            Home
+        </li>
+        <li>
+            About
+        </li>
+        <li>
+            Reservations
+        </li>
+        <li>
+            Menu
+        </li>
+        <li>
+            Order Online
+        </li>
+        <li>
+            Login
+        </li>
+
+    </ul>
+</nav>
